e43d9fcb975c9851b01d28c0812e1dc86eed537a
Spencer Presley <153392395+SpencerPresley@users.noreply.github.com>
2025-03-01 13:34:16 -0500

    diff_iculty

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from fastapi.templating import Jinja2Templates
 
 class UserForm(BaseModel):
     course_work: str
-    difficulty: str
+    diff_iculty: str
     school_level: str
     subject: str
     subject_material: List[UploadFile]
