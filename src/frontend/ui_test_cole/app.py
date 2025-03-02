e1ad95853500c3287a7eca946c7d42f91637cd35
cbarbes1 <cbarbes02@gmail.com>
2025-03-01 20:29:44 -0500

    added code to the download and flashcard page

--- src/frontend/ui_test_cole/app.py
+++ src/frontend/ui_test_cole/app.py
@@ -15,6 +15,10 @@ def index(filename):
 def serve_static(filename):
     return send_from_directory('public', filename)
 
+@app.route('/styles/<path:filename>')
+def serve_styles(filename):
+    return send_from_directory('styles', filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
