8a5b3c3aaf718c167fc67372d857395c63f432e2
cbarbes1 <cbarbes02@gmail.com>
2025-03-01 19:58:06 -0500

    flash cards

---
+++ src/frontend/ui_test_cole/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/flashcards/<filename>')
+def get_flashcards(filename):
+    return render_template('index.html', filename=filename)
+
+@app.route('/<filename>')
+def index(filename):
+    print(filename)
+    return render_template('index.html', filename=filename)
+
+@app.route('/public/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('public', filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
